cf23c856527e44a23102f42de7d41da0e68dbbe3
Constantine Korikov <constantine.korikov+github@gmail.com>
2019-09-23 22:36:04 +0300

    feat(polytech): add binary and  interpolation search

--- polytech/algorithm/search.py
+++ polytech/algorithm/search.py
@@ -1,8 +1,36 @@
-"""Алгоритмы поиска"""
+'''Алгоритмы поиска'''
 
-# Линейный поиск
 def linear(val, array):
+    '''Линейный поиск'''
     for i, x in enumerate(array):
         if x == val:
             return i
+    return -1
+
+def binary(val, array):
+    '''Бинарный поиск'''
+    low = 0
+    high = len(array)-1
+    while low <= high:
+        m = low + (high-low)//2
+        if array[m] == val:
+            return m
+        elif array[m] < val:
+            low = m+1
+        else: 
+            high = m-1
+    return -1
+
+def interpolation(val, array):
+    '''Интерполяционный поиск (только для числовых данных)'''
+    low = 0
+    high = len(array)-1
+    while low <= high and val >= array[low] and val <= array[high]:
+        m = low + (high-low)*(val-array[low])//(array[high]-array[low])
+        if array[m] == val:
+            return m
+        elif array[m] < val:
+            low = m+1
+        else: 
+            high = m-1
     return -1
